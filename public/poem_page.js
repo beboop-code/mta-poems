b6a0a51602daaee7e78b0c2cc52d48ccff428f81
beboop-code <ally.ga892@gmail.com>
2025-11-24 10:59:24 -0500

    test push

--- public/poem_page.js
+++ public/poem_page.js
@@ -5,14 +5,6 @@ async function setup() {
 
 }
 function fetchNewPoem() {
-    
-    // let num=random(0,100);
-    // text(num,50,50);
-    // noStroke();
-    // fill(255);
-    // rect(40,40,30,30);
-    // fill(255,0,0);
-    // text(num,50,50);
     const poemDisplay = document.getElementById('poemDisplay');
     fetch('/random-poem')
         .then(response => response.text())
@@ -34,13 +26,3 @@ function fetchNewPoem() {
 
 
 }
-// const getter = fetch('/random-poem', { // Your Node.js endpoint
-//           method: 'GET',
-//           headers: {
-//               'Accept': 'application/json',
-//               'Content-Type': 'application/json'
-//               },
-//           //body: JSON.stringify({"train": poemText.value, "poem": nameField.value()})
-//           //body: JSON.stringify({"train":  nameField.value(), "poem": poemText.value})
-//           body: JSON.stringify({"date": date,"time":time,"train":  trainText.value, "poem": poemText.value})
-//       });
